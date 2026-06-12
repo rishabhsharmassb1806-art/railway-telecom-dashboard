f0904684f427c320c06a4f73e07a728a34c93561
Rishi <rishabhsharmassb1806@gmail.com>
2026-06-12 16:57:29 +0530

    Added section and closing date fields

--- Backend/models/failure.js
+++ Backend/models/failure.js
@@ -5,22 +5,39 @@ const failureSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
   location: {
     type: String,
     required: true,
   },
+
+  section: {
+    type: String,
+    default: "",
+  },
+
+  closingDate: {
+    type: String,
+    default: "",
+  },
+
   severity: {
     type: String,
     default: "Low",
   },
+
   status: {
     type: String,
     default: "Open",
   },
+
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
 
-module.exports = mongoose.model("Failure", failureSchema);
+module.exports = mongoose.model(
+  "Failure",
+  failureSchema
+);
